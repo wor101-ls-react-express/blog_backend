395c0bd33333273a0f5b0f3bf3b79d564086c67e
Will <wor101@gmail.com>
2021-12-14 22:32:57 -0500

    added users, token authentication, and more

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const mongoose = require('mongoose')
 const Blog = require('./models/blogs')
 
@@ -16,7 +19,10 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,27 +1,48 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7);
+//   }
+//   return null;
+// };
+
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
-  console.log(blogs);
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 });
+
   response.json(blogs);
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
+  const token = request.token   // getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
   });
 
   try {
-    const result = await blog.save();
+    const savedBlog = await blog.save();
     console.log('Successfully sent blog post');
-    console.log(result);
-    response.status(201).json(result);
+    console.log(savedBlog);
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save();
+    response.status(201).json(savedBlog);
   } catch(error) {
     next(error);
   }
